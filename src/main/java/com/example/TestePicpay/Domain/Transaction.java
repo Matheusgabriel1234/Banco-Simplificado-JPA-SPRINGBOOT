93046d9f752cc5687c95b51b724757c47910b04a
Matheusgabriel1234 <magaje2005@gmail.com>
2024-05-27 13:51:24 -0300

    User Service e tratamento de exceções basicas

--- src/main/java/com/example/TestePicpay/Domain/Transaction.java
+++ src/main/java/com/example/TestePicpay/Domain/Transaction.java
@@ -1,5 +1,38 @@
 package com.example.TestePicpay.Domain;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "tb_Transactions")
 public class Transaction {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+private BigDecimal amount;
+@ManyToOne
+@JoinColumn(name = "sender_id")
+private User sender;
+@ManyToOne
+@JoinColumn(name = "receiver_id")
+private User receiver;
+
 
 }
--- src/main/java/com/example/TestePicpay/Domain/User.java
+++ src/main/java/com/example/TestePicpay/Domain/User.java
@@ -1,5 +1,60 @@
 package com.example.TestePicpay.Domain;
 
-public class User {
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "tb_users")
+@Entity
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User implements Serializable {
+
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Getter
+@Setter
+private Long id;
+@Getter
+@Setter
+private String firstName;
+@Getter
+@Setter
+private String secondName;
+@Column(unique = true)
+private String cpf;
+@Column(unique = true)
+private String email;
+@Getter
+@Setter
+private String password;
+@Getter
+@Setter
+private BigDecimal balance;
+@Enumerated(EnumType.STRING)
+private UserType userType;
+
+
+
+
 
 }
